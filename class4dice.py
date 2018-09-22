f2ba9bca7a19f4e28d01965f95c7fdb2c9e2e093
kamyarachel <43031674+kamyarachel@users.noreply.github.com>
2018-09-22 10:41:34 +0530

    Add files via upload

---
+++ class4dice.py
@@ -0,0 +1,10 @@
+while True:
+	n=int(input("enter 0 to quit,or anyother number"))
+
+	if n==0:
+		print("bye...")
+		exit()
+	else:
+		print("you have entered:",n)	
+
+	
---
+++ class4largest3.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+if a>b and a>c:
+	print(a,"is the greater number")
+elif b>c:
+	print(b,"is the greater number")
+else:
+	print(c,"is the greater number")
