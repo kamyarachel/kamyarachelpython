b31003e777d1feaa26ec187bb5fb298661f2a592
kamyarachel <43031674+kamyarachel@users.noreply.github.com>
2018-10-04 16:17:56 +0530

    Add files via upload

--- class6stonepaper.py
+++ class6stonepaper.py
@@ -1,26 +1,43 @@
 import random
-p1={1:'r',2:'p',3:'s'}
-r=random.randint(1,3)
-print(r)
-while True:
-	p2=input("enter you choice r/p/s")
+a=0
+b=0
+while a<3 and b<3:
+    r={1:'r',2:'p',3:'s'}
+    comp=r[random.randint(1,3)]
+    print(comp)
+    p2=input("enter you choice r/p/s or q to quit")
+	if p2=='q':
+			print("bye")
+			exit()
 	if p2==p1:
 		print("tie!!")
-	elif p2==r:
-		if p1==p:
+	elif p2=='r':
+		if p1=='p':
 			print("player 1 got the point")
-	else:
+			a=a+1
+		else:
 			print("player 2 got the point")
-	elif p2==p:
-		if p1==r:
+			b=b+1
+	elif p2=='p':
+		if p1=='r':
 			print("player 2 got the point")
-	else:
+			b=b+1
+		else:
 			print("player 1 got the point")
-	elif p2==s:
-		if p1==p:
+			a=a+1
+	elif p2=='s':
+		if p1=='p':
 			print("player 2 got the point")
-	else:
+			b=b+1
+		else:
 			print ("player 1 got the point")
+			a=a+1
+	if a==3:
+		print("u won")
+	elif b==3:
+		print("comp won")
+
+
 
 
 
